6571c284f89bd00a728711d053603e77b277ddf7
Vic <Vic@gmail.com>
2021-06-11 12:10:19 +0800

    [ add ]驗證頁

--- src/router/index.js
+++ src/router/index.js
@@ -15,17 +15,19 @@ const routes = [
       {
         path: '/about',
         name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
       },
       {
         path: '/test',
         name: 'Test',
-        component: () => import(/* webpackChunkName: "about" */ '../views/Test.vue'),
+        component: () => import('../views/Test.vue'),
       },
     ],
+  },
+  {
+    path: '/manage',
+    name: 'Dashboard',
+    component: () => import('../views/backEnd/Dashboard.vue'),
   },
 ];
 
