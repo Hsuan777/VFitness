e2050115e12f4bf452d02186006e78ab669b5e49
Vic <Vic@gmail.com>
2021-06-24 09:59:41 +0800

    [ add ] 首頁訂閱我們

--- src/views/frontend/Home.vue
+++ src/views/frontend/Home.vue
@@ -1,12 +1,11 @@
 <template>
   <!-- banner -->
-  <figure class="position-relative mb-5">
-    <img src="https://images.unsplash.com/photo-1558611848-73f7eb4001a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=1080" alt="維克健身餐飲複合工作室" class="studio__banner">
-    <div class="position-absolute top-50 start-25 translate-middle mt-5 ps-4">
-      <h1 class=" text-white display-3 fw-bolder mb-3">
+  <figure class="studio__banner d-flex align-items-center mb-5">
+    <div class="container mt-5">
+      <h1 class="text-white display-3 fw-bolder mb-3">
         維克健身餐飲<br>複合工作室
       </h1>
-      <button type="button" class="btn btn-primary"><span class="display-6">立即體驗</span></button>
+      <button type="button" class="btn btn-primary"><span class="display-7">立即體驗</span></button>
     </div>
   </figure>
   <!-- 工作室目標 -->
@@ -101,6 +100,23 @@
       </div>
     </div>
   </section>
+  <!-- 訂閱我們 -->
+  <figure class="studio__subscribe d-flex align-items-center mb-0">
+    <div class="container mt-5">
+      <div class="col-lg-6">
+        <p class="h3 text-white">
+          請訂閱我們<br>
+          <span class="h4">分享健身小知識，折扣代碼不錯過。</span>
+        </p>
+      </div>
+      <div class="col-lg-4">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="輸入電子信箱">
+          <button class="btn btn-primary" type="button" @click="clickSubscribe">送出</button>
+        </div>
+      </div>
+    </div>
+  </figure>
 </template>
 
 <script>
@@ -188,6 +204,9 @@ export default {
       });
       this.randomData.push(food[this.randomNumber(food.length, 0)]);
       this.randomData.push(course[this.randomNumber(course.length, 0)]);
+    },
+    clickSubscribe() {
+      this.swal('感謝您的訂閱');
     },
   },
   created() {
