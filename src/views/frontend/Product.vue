97e5a91df7d21eab18d35c4f6f869738a97ce72e
Vic <Vic@gmail.com>
2021-06-24 13:41:07 +0800

    [ add ] 產品頁加入訂閱區塊

--- src/views/frontend/Product.vue
+++ src/views/frontend/Product.vue
@@ -125,6 +125,23 @@
       </li>
     </ul>
   </section>
+  <!-- 訂閱我們 -->
+  <figure class="studio__subscribe d-flex align-items-center mb-0">
+    <div class="container mt-5">
+      <div class="col-lg-6">
+        <p class="h3 text-white">
+          請訂閱我們<br>
+          <span class="h4">分享健身小知識，折扣代碼不錯過。</span>
+        </p>
+      </div>
+      <div class="col-lg-4">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="輸入電子信箱">
+          <button class="btn btn-primary" type="button" @click="clickSubscribe">送出</button>
+        </div>
+      </div>
+    </div>
+  </figure>
 </template>
 
 <script>
@@ -252,6 +269,9 @@ export default {
         this.randomData.push(this.products[y]);
         tempData.splice(y, 1);
       }
+    },
+    clickSubscribe() {
+      this.swal('感謝您的訂閱');
     },
   },
   created() {
