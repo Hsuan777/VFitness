b89993c897ec412d3905305fe5481c73e8c4d2dc
Vic <Vic@gmail.com>
2021-06-28 14:36:56 +0800

    [ edit ] 讓 Toast 抓不同資料

--- src/components/tools/Toast.vue
+++ src/components/tools/Toast.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="studio__toast__position toast-container position-fixed pb-4" style="z-index: 999">
     <div
-      v-for="item in tempMessage"
+      v-for="item in toastIndex"
       :key="item"
       :ref="`liveToast${item}`"
       class="toast hide bg-white border border-secondary rounded"
@@ -9,17 +9,25 @@
       aria-live="assertive"
       aria-atomic="true"
     >
-      <div class="toast-header">
-        <strong class="h4 mb-0 me-3">通知訊息</strong>
-        <small class="ms-auto">{{date}}</small>
-        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-      </div>
-      <div
-        class="toast-body text-center h6 mb-0"
-        :class="status === 'error' ? 'text-danger' : 'text-success'"
-      >
-        {{ msg }}
-      </div>
+      <!-- 若不使用 v-if，vue 抓不到會報錯 -->
+      <template v-if="toastArray[item - 1]">
+        <div class="toast-header">
+          <strong class="h4 mb-0 me-3">通知訊息</strong>
+          <small class="ms-auto">{{ toastArray[item - 1].date }}</small>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="toast"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div
+          class="toast-body text-center h6 mb-0"
+          :class="toastArray[item - 1].status === 'error' ? 'text-danger' : 'text-success'"
+        >
+          {{ toastArray[item - 1].msg }}
+        </div>
+      </template>
     </div>
   </div>
 </template>
@@ -30,21 +38,23 @@ import Toast from 'bootstrap/js/dist/toast';
 export default {
   data() {
     return {
-      toast: {},
-      msg: '',
-      status: '',
-      date: '',
-      tempMessage: 1,
+      toastTarget: {},
+      toastIndex: 1,
+      toastArray: [],
     };
   },
   methods: {
-    showToast(message, status) {
-      this.msg = message;
-      this.status = status;
-      this.date = new Date().toLocaleString();
-      this.toast = new Toast(this.$refs[`liveToast${this.tempMessage}`]);
-      this.tempMessage += 1;
-      this.toast.show();
+    showToast(message, toastStatus) {
+      const nowDate = new Date().toLocaleString();
+      const toastInfo = {
+        msg: message,
+        status: toastStatus,
+        date: nowDate,
+      };
+      this.toastArray.push(toastInfo);
+      this.toastTarget = new Toast(this.$refs[`liveToast${this.toastIndex}`]);
+      this.toastIndex += 1;
+      this.toastTarget.show();
     },
     hideToast() {
       this.toast.hide();
